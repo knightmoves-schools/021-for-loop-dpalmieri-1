92fc83fa73f04d07d9b5b106d97231a1ae0e1d65
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-05 17:38:08 +0000

    Loops are so interesting.

--- ToDo.cs
+++ ToDo.cs
@@ -2,4 +2,13 @@
 public class ToDo
 {
     // Add your code here
+    public string MarkAsDone(string[] todos)
+    {
+        int i = 0;
+        for(i = 0; i < todos.Length; i++)
+        {
+            return "done - " + todos[i];
+        }
+    }
+
 }
